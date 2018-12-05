1b886ab8dea002894df217ffa5403b39bc13a0cf
maximeriere <maxime.riere@telecomnancy.net>
2018-12-05 12:50:40 +0100

    avancee

--- Protein/src/main/java/Main.java
+++ Protein/src/main/java/Main.java
@@ -1,14 +1,22 @@
 import Neo.Neo4j;
 import UniProt.Protein;
 import UniProt.Utils;
+//import jdk.tools.jlink.internal.Platform;
 import uk.ac.ebi.kraken.interfaces.uniprot.comments.Comment;
 import uk.ac.ebi.kraken.xml.jaxb.uniprot.Uniprot;
 import uk.ac.ebi.uniprot.dataservice.client.exception.ServiceException;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
@@ -22,12 +30,13 @@ public class Main {
         neo4j.init();
         int numberNeighbor = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("----------------Welcome Uniprot--------------");
+        /*System.out.println("------------------------------");
         System.out.println("---- 1 . Search By Id -----------------------");
         System.out.println("---- 2 . Search By Name----------------------");
         System.out.print("Entre your number : ");
         int choise = sc.nextInt();
-        switch (choise){
+
+         switch (choise){
             case 1 :
                 System.out.print("Entre your ID : ");
                 String id = sc.next();
@@ -42,7 +51,40 @@ public class Main {
                 numberNeighbor = sc.nextInt();
                 prot = Utils.getProteinDetailsNAME(name);
                 break;
+        }*/
+
+        //interface graphique ma ptite gueule
+        String[] sexe = {"Par ID", "Par Nom"};
+        JOptionPane jop = new JOptionPane();
+        String selectedValue = (String)jop.showInputDialog(null,
+                "Veuillez choisir votre moyen de recherche de proteine !",
+                "Protein Searcher",
+                JOptionPane.QUESTION_MESSAGE,
+                new ImageIcon("ProteinSpecificityCode.jpg"),
+                sexe,
+                sexe[0]);
+
+        if (selectedValue.equals("Par ID")) {
+            System.out.println("id");
+            JOptionPane jopid = new JOptionPane();
+            String id = jopid.showInputDialog(null, "Saisissez l'ID de la protéine recherchée", "Protein Searcher", JOptionPane.QUESTION_MESSAGE);
+            prot = Utils.getProteinDetailsID(id);
+        } else if (selectedValue.equals("Par Nom")) {
+            System.out.println("nom");
+            JOptionPane jopnom = new JOptionPane();
+            String name = jopnom.showInputDialog(null, "Saisissez le nom de la protéine recherchée", "Protein Searcher", JOptionPane.QUESTION_MESSAGE);
+            prot = Utils.getProteinDetailsNAME(name);
         }
+        JOptionPane jopnei = new JOptionPane();
+        String nei = jopnei.showInputDialog(null, "Saisissez le nombre de voisins recherchés", "Protein Searcher", JOptionPane.QUESTION_MESSAGE);
+        numberNeighbor=Integer.parseInt(nei);
+
+
+
+
+
+
+
         // get Neighbors
         Protein protein = prot;
 
